03e8376ca4778d6c175211468bb9bd908cfa3c1b
Min Ç2 <minsethu619@gmail.com>
2021-11-23 22:40:24 +0800

    Added a function to open the database connection

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+
 	_ "github.com/go-sql-driver/mysql"
 )
+
+func main() {
+	db, err := sql.Open("mysql", "user:password@tcp(127.0.0.1:3306)/Passengers")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Println("Database opened")
+
+	defer db.Close()
+}
