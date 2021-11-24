adc3e0ff2002f987db1121049f0f6cdf50bfc6aa
Min Ç2 <minsethu619@gmail.com>
2021-11-24 13:55:37 +0800

    Added code to display Passenger table.

--- main.go
+++ main.go
@@ -7,14 +7,40 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func main() {
-	db, err := sql.Open("mysql", "user:password@tcp(127.0.0.1:3306)/Passengers")
+type Passengers struct {
+	PassengerID int
+	FirstName   string
+	LastName    string
+	PhoneNumber int
+	Email       string
+}
+
+func getRecords(db *sql.DB) {
+	results, err := db.Query("Select * FROM ETIAsgn.Passengers")
 
 	if err != nil {
 		panic(err.Error())
 	}
 
-	fmt.Println("Database opened")
+	for results.Next() {
+		var passenger Passengers
+		err = results.Scan(&passenger.PassengerID, &passenger.FirstName, &passenger.LastName, &passenger.PhoneNumber, &passenger.Email)
 
+		if err != nil {
+			panic(err.Error())
+		}
+		fmt.Println(passenger.PassengerID, passenger.FirstName, passenger.LastName, passenger.PhoneNumber, passenger.Email)
+	}
+}
+
+func main() {
+	db, err := sql.Open("mysql", "user:password@tcp(127.0.0.1:3306)/Passengers")
+
+	if err != nil {
+		panic(err.Error())
+	} else {
+		fmt.Println("Database opened")
+	}
+	getRecords(db)
 	defer db.Close()
 }
