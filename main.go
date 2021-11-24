a9b6002fc1ebc7841dfa584584cc460ca4283e66
Min Ç2 <minsethu619@gmail.com>
2021-11-24 16:57:19 +0800

    Added code to display Trip table

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+//Collections of fields for Passengers
 type Passengers struct {
 	PassengerID int
 	FirstName   string
@@ -15,6 +16,7 @@ type Passengers struct {
 	Email       string
 }
 
+//Collections of fields for Drivers
 type Drivers struct {
 	DriverID    int
 	FirstName   string
@@ -24,8 +26,18 @@ type Drivers struct {
 	LicenseNo   int
 }
 
+//Collections of fields for trips
+type Trip struct {
+	PostalCode  int
+	Pickup      string
+	Dropoff     string
+	DriverID    int
+	PassengerID int
+}
+
+//Function to get the passenger table from the database
 func getPassengerRecords(db *sql.DB) {
-	results, err := db.Query("Select * FROM ETIAsgn.Passengers")
+	results, err := db.Query("SELECT * FROM ETIAsgn.Passengers")
 
 	if err != nil {
 		panic(err.Error())
@@ -42,8 +54,9 @@ func getPassengerRecords(db *sql.DB) {
 	}
 }
 
+//function to get the driver table from the database
 func getDriverRecords(db *sql.DB) {
-	results, err := db.Query("Select * FROM ETIAsgn.Drivers")
+	results, err := db.Query("SELECT * FROM ETIAsgn.Drivers")
 
 	if err != nil {
 		panic(err.Error())
@@ -60,6 +73,25 @@ func getDriverRecords(db *sql.DB) {
 	}
 }
 
+//function to get the trip table from the database
+func getTripRecords(db *sql.DB) {
+	results, err := db.Query("SELECT * FROM ETIAsgn.Trip")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for results.Next() {
+		var trip Trip
+		err = results.Scan(&trip.PostalCode, &trip.Pickup, &trip.Dropoff, &trip.DriverID, &trip.PassengerID)
+
+		if err != nil {
+			panic(err.Error())
+		}
+		fmt.Println(trip.PostalCode, trip.Pickup, trip.Dropoff, trip.DriverID, trip.PassengerID)
+	}
+}
+
 func main() {
 	db, err := sql.Open("mysql", "user:password@tcp(127.0.0.1:3306)/Passengers")
 
@@ -68,7 +100,9 @@ func main() {
 	} else {
 		fmt.Println("Database opened")
 	}
+
 	getPassengerRecords(db)
 	getDriverRecords(db)
+	getTripRecords(db)
 	defer db.Close()
 }
